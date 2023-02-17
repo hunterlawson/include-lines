82b9c7d78ad0b43122b6c0b4ca9a267f4f22796a
Hunter Lawson <hunter.lawson@ufl.edu>
2023-02-17 03:37:36 -0500

    Add public versions of the static include macros

--- include-lines/src/lib.rs
+++ include-lines/src/lib.rs
@@ -105,6 +105,16 @@ macro_rules! static_include_lines {
     };
 }
 
+/// This macro functions identically to [static_include_lines], but the created static variable is public.
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! pub_static_include_lines {
+    ($var: ident, $filename: literal) => {
+        pub static $var: [&str; include_lines::count_lines!($filename)] =
+            include_lines::include_lines!($filename);
+    };
+}
+
 /// This macro functions identically to [static_include_lines], but each line is returned as type `String`.
 #[allow(unused_macros)]
 #[macro_export]
@@ -114,3 +124,14 @@ macro_rules! static_include_lines_s {
             include_lines::include_lines_s!($filename);
     };
 }
+
+/// This macro functions identically to [static_include_lines_s], but the created static variable is public.
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! pub_static_include_lines_s {
+    ($var: ident, $filename: literal) => {
+        pub static $var: [String; include_lines::count_lines!($filename)] =
+            include_lines::include_lines_s!($filename);
+    };
+}
+
